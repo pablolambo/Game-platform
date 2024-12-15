d3be53577afc3d4dd2e83e886692c712e5a9f2f0
Paul Frank <pawelfrankowski42@gmail.com>
2024-12-15 18:48:11 +0100

    Solution items and catalogs

--- src/VgcZeroGamePlatform.Api/Program.cs
+++ src/VgcZeroGamePlatform.Api/Program.cs
@@ -1,6 +1,16 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using VgcZeroGamePlatform.Domain.Entities;
+using VgcZeroGamePlatform.Infrastructure.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<GameStoreContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<GameStoreContext>();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
